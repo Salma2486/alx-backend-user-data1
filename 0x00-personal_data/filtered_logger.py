9e60d02e9da228a8f44a9443686aebb0ad354c30
Salma2486 <138704540+Salma2486@users.noreply.github.com>
2024-07-04 20:05:11 +0000

    y

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,10 +3,7 @@
 """
 import re
 
+
 def filter_datum(fields, redaction, message, separator):
     pattern = '|'.join([f'{field}=[^ {separator}]+' for field in fields])
     return re.sub(pattern, lambda m: f"{m.group().split('=')[0]}={redaction}", message)
-
-
-if __name__ == "__main__":
-    main()
